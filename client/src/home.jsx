ba0f7b3e45187ac71001052789e8e053caf5b372
unknown <aliopdandan@gmail.com>
2024-07-10 09:01:44 +0100

    f

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import pic1 from "./assets/c-d-x-PDX_a_82obo-unsplash.jpg"; // Assuming the file is a .jpg
-import pic2 from "./assets/domino-studio-164_6wVEHfI-unsplash.jpg"; // Assuming the file is a .jpg
-import pic3 from "./assets/eniko-kis-KsLPTsYaqIQ-unsplash.jpg"; // Assuming the file is a .jpg
-import pic4 from "./assets/giorgio-trovato-K62u25Jk6vo-unsplash.jpg"; // Assuming the file is a .jpg
-import pic5 from "./assets/irene-kredenets-KStSiM1UvPw-unsplash.jpg"; // Assuming the file is a .jpg
-import pic6 from "./assets/joan-tran-reEySFadyJQ-unsplash.jpg"; // Assuming the file is a .jpg
-import pic7 from "./assets/kiran-ck-LSNJ-pltdu8-unsplash.jpg"; // Assuming the file is a .jpg
-import pic8 from "./assets/rachit-tank-2cFZ_FB08UM-unsplash.jpg"; // Assuming the file is a .jpg
+import pic1 from "./assets/c-d-x-PDX_a_82obo-unsplash.jpg";
+import pic2 from "./assets/domino-studio-164_6wVEHfI-unsplash.jpg";
+import pic3 from "./assets/eniko-kis-KsLPTsYaqIQ-unsplash.jpg";
 
 export function Home() {
-  const pictures = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
+  const pictures = [pic1, pic2, pic3];
 
   return (
-    <div>
+    <>
+    <div className="flex justify-between bg-slate-800 text-white p-4 mb-4 items-center">
+        <div>
+            <h1>E-Commerce Store</h1>
+        </div>
+        <div className="flex gap-4">
+            <button className="bg-sky-500 text-white px-5 py-2 rounded-md mt-2 hover:bg-sky-900">Log in</button>
+            <button className="bg-sky-500 text-white px-5 py-2 rounded-md mt-2 hover:bg-sky-900">sign up</button>
+        </div>
+    </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {pictures.map((pic, index) => (
-        <img src={pic} alt={`Image ${index + 1}`} key={index} className="max-w-52"/>
+        <div key={index} className="border border-black p-2 flex flex-col items-center">
+          <img src={pic} alt={`Image ${index + 1}`} className="w-full h-auto max-w-full max-h-full" />
+          <button className="bg-sky-500 text-white px-16 py-2 rounded-md mt-2 hover:bg-sky-900">Add to chart</button>
+        </div>
       ))}
     </div>
+    </>
   );
 }
